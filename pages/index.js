763f256ee85a8afeb1b8ec679ad75655fbe4bac6
Thomas Kirst <thomas.kirst@neuefische.de>
2022-09-06 14:43:13 +0200

    Add externally referenced image from unsplash

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import Image from 'next/image';
+
 export default function Home() {
-  return <h1>Main Content on the index page (/)</h1>;
+  return (
+    <>
+      <h1>Main Content on the index page (/)</h1>
+      <Image
+        src="https://images.unsplash.com/photo-1588276552401-30058a0fe57b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format"
+        width={5670}
+        height={4000}
+        alt="Paella"
+      />
+    </>
+  );
 }

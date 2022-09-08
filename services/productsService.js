43eb09ccacd5d444b1e500d58ee154db7eb2ac77
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2022-09-08 11:44:34 +0200

    Update product service functions with database connection

--- services/productsService.js
+++ services/productsService.js
@@ -1,58 +1,26 @@
-const products = [
-  {
-    id: 1,
-    name: 'Garnele',
-    description: 'Lebt bevorzugt paarweise',
-    price: 19,
-    category: 'Wirbellose',
-  },
-  {
-    id: 2,
-    name: 'Anemonenfisch',
-    description: 'Nemo',
-    price: 60,
-    category: 'Meerwasser',
-  },
-  {
-    id: 3,
-    name: 'Pracht-Anemone',
-    description: 'Ist prächtig',
-    price: 90,
-    category: 'Korallen und Blumentiere',
-  },
-  {
-    id: 4,
-    name: 'Mördermuschel',
-    description: 'Ist hübscher als ihr Name',
-    price: 125,
-    category: 'Muscheln',
-  },
-  {
-    id: 5,
-    name: 'Kaiserfisch',
-    description: 'Ein tagaktiver Einzelgänger',
-    price: 90,
-  },
-  {
-    id: 6,
-    name: 'Guppy',
-    description: 'Klein aber fein',
-    price: 5,
-    category: 'Süsswasser',
-  },
-  {
-    id: 7,
-    name: 'Regenbogenfisch',
-    description: 'In allen Farben und bunt',
-    price: 12,
-    category: 'Süsswasser',
-  },
-];
+import dbConnect from "../dbConnect";
+import Product from "../models/Product";
 
-export function getAllProducts() {
-  return products;
+export async function getAllProducts() {
+  await dbConnect(); // Connect to database
+
+  const products = await Product.find(); // Find collection "products" and return all values
+
+  const productArray = products.map(
+    ({ id, name, description, price, category }) => {
+      return { id, name, description, price, category };
+    }
+  );
+
+  return productArray;
 }
 
-export function getProductById(id) {
-  return products.find((product) => product.id === id);
+export async function getProductById(productId) {
+  await dbConnect();
+
+  const product = await Product.findById(productId);
+
+  const { id, name, description, price, category } = product;
+
+  return { id, name, description, price, category };
 }

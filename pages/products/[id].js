a7446c1232ec38c993ecb51929af52080610485b
Marie-Carleen Frei <mariecarleen.koizumi@gmail.com>
2022-09-08 06:47:47 +0200

    Add form to update individual product

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,19 +1,31 @@
-import Head from 'next/head';
-import { getProductById } from '../../services/productsService';
+import Head from "next/head";
+import ProductForm from "../../components/ProductForm";
+import { getAllCategories } from "../../services/categoriesService";
+import { getProductById } from "../../services/productsService";
 
 export function getServerSideProps(context) {
   const { id } = context.params;
+  const categories = getAllCategories();
   const product = getProductById(Number(id));
 
   return {
     props: {
       product,
+      categories,
     },
   };
 }
 
-export default function Product({ product }) {
+export default function Product({ product, categories }) {
   const { id, name, description } = product;
+
+  function updateProduct(updatedProduct) {
+    console.log(updatedProduct);
+  }
+
+  function deleteProduct() {
+    console.log("deleted!");
+  }
 
   return (
     <>
@@ -24,6 +36,14 @@ export default function Product({ product }) {
       <p>ID: {id}</p>
       <h2>Beschreibung</h2>
       <p>{description}</p>
+      <ProductForm
+        onHandleSubmit={updateProduct}
+        categories={categories}
+        product={product}
+      />
+      <button type="button" onClick={deleteProduct}>
+        Produkt löschen
+      </button>
     </>
   );
 }
